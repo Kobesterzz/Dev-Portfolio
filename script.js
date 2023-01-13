7da68d5ec76f973925e567dd40bfe4a9e5bd3f65
kobe shaw <kobeshaw330@gmail.com>
2023-01-12 19:00:56 -0600

    I almost finished the landing page,i added two orbs in the background with the filter and i also added the background color changer

--- script.js
+++ script.js
@@ -3,15 +3,8 @@ let angle = 0;
 
 setInterval(() => {
   angle = (angle + 1) % 360;
-  gradient.style.background = `linear-gradient(${angle}deg, #ff0000, #ffff00)`;
+  gradient.style.background = `linear-gradient(${angle}deg, #16161a, )`;
 }, 50);
-
-const object = document.getElementById("object");
-
-document.addEventListener("mousemove", (event) => {
-  object.style.left = `${event.clientX - 25}px`;
-  object.style.top = `${event.clientY - 25}px`;
-});
 
 const object2 = document.getElementById("object2");
 
@@ -19,4 +12,21 @@ document.addEventListener("mousemove", (event) => {
   object2.style.left = `${event.clientX - 25}px`;
   object2.style.top = `${event.clientY - 25}px`;
 });
+
+const object3 = document.getElementById("object3");
+
+document.addEventListener("mousemove", (event) => {
+  object3.style.left = `${event.clientX - 25}px`;
+  object3.style.top = `${event.clientY - 25}px`;
+});
+
+const colorChanger = document.getElementById("colorChanger");
+
+function myFunction(){   
+document.body.style.backgroundColor= "white";
+document.getElementById("filter").style.opacity = .02;
+document.getElementById('line1').style.color='rgb(255, 0, 255)';
+document.getElementById('line3').style.color='rgb(255, 0, 255)';
+document.getElementById('paragraph').style.color='rgb(255, 0, 255)';
+};
 
