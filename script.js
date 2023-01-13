93953b64fe96a8d696de549ba8bfd283b6c17cbf
kobe shaw <kobeshaw330@gmail.com>
2023-01-13 08:53:09 -0600

    added a modal that transitions in when you click the hamburger button, and fixed some minor details.

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ setInterval(() => {
 const object2 = document.getElementById("object2");
 
 document.addEventListener("mousemove", (event) => {
-  object2.style.left = `${event.clientX - 25}px`;
-  object2.style.top = `${event.clientY - 25}px`;
+  object2.style.left = `${event.pageX - 25}px`;
+  object2.style.top = `${event.pageY - 25}px`;
 });
 
 const object3 = document.getElementById("object3");
 
 document.addEventListener("mousemove", (event) => {
-  object3.style.left = `${event.clientX - 25}px`;
-  object3.style.top = `${event.clientY - 25}px`;
+  object3.style.left = `${event.pageX - 25}px`;
+  object3.style.top = `${event.pageY - 25}px`;
 });
 
 const colorChanger = document.getElementById("colorChanger");
@@ -28,5 +28,30 @@ document.getElementById("filter").style.opacity = .02;
 document.getElementById('line1').style.color='rgb(255, 0, 255)';
 document.getElementById('line3').style.color='rgb(255, 0, 255)';
 document.getElementById('paragraph').style.color='rgb(255, 0, 255)';
+}
+
+
+window.onload = () => {
+  // Your JavaScript code here
+  const hamburgerButton = document.getElementById("hamburger-button");
+  const modal = document.getElementById("modal");
+  const closeButton = document.getElementById("close-button");
+  
+  hamburgerButton.addEventListener("click", () => {
+    modal.classList.add("modal-open");
+    modal.classList.add("modal-animation");
+  });
+  
+  closeButton.addEventListener("click", () => {
+    modal.classList.remove("modal-open");
+    modal.classList.add("modal-animation-out");
+  });
+
+  modal.addEventListener("transitionend", () => {
+    modal.classList.remove("modal-animation-out");
+  });
 };
+
+
+
 
