a522c711e144b1cfd23315364803e1e179d649ca
kobe shaw <kobeshaw330@gmail.com>
2023-02-28 08:20:38 -0600

    changed accent colors

--- script.js
+++ script.js
@@ -27,26 +27,26 @@ function Dark(){
   if(localStorage.getItem('Dark') === 'true'){
   document.body.style.backgroundColor= "#1c1c2c";
   document.getElementById("filter").style.opacity = .55;
-  document.getElementById('line2').style.color='aqua';
+  document.getElementById('line2').style.color='rgb(0, 255, 166)';
   document.getElementById('line1').style.color='white';
   document.getElementById('line3').style.color='white';
   document.getElementById('paragraph').style.color='white';
   document.getElementById('modal').style.backgroundColor='rgba(50, 48, 69, 0.994)';
   document.getElementById('wannaTalk').style.color='white';
-  document.getElementById('overlayArrow2').style.color='aqua';
-  document.getElementById('overlayArrow1').style.color='aqua';
-  document.getElementById('overlayTitle1').style.color='aqua';
-  document.getElementById('overlayTitle2').style.color='aqua';
-  document.getElementById('ViewMore1').style.color='aqua';
-  document.getElementById('ViewMore2').style.color='aqua';
+  document.getElementById('overlayArrow2').style.color='rgb(0, 255, 166)';
+  document.getElementById('overlayArrow1').style.color='rgb(0, 255, 166)';
+  document.getElementById('overlayTitle1').style.color='rgb(0, 255, 166)';
+  document.getElementById('overlayTitle2').style.color='rgb(0, 255, 166)';
+  document.getElementById('ViewMore1').style.color='rgb(0, 255, 166)';
+  document.getElementById('ViewMore2').style.color='rgb(0, 255, 166)';
   document.getElementById('footer-name').style.color='white';
-  document.getElementById('title1').style.color='aqua';
-  document.getElementById('title2').style.color='aqua';
-  document.getElementById('contact').style.color='aqua';
-  document.getElementById('modalHom').style.color='aqua';
-  document.getElementById('modalAbo').style.color='aqua';
-  document.getElementById('modalPro').style.color='aqua';
-  document.getElementById('modalCon').style.color='aqua';
+  document.getElementById('title1').style.color='rgb(0, 255, 166)';
+  document.getElementById('title2').style.color='rgb(0, 255, 166)';
+  document.getElementById('contact').style.color='rgb(0, 255, 166)';
+  document.getElementById('modalHom').style.color='rgb(0, 255, 166)';
+  document.getElementById('modalAbo').style.color='rgb(0, 255, 166)';
+  document.getElementById('modalPro').style.color='rgb(0, 255, 166)';
+  document.getElementById('modalCon').style.color='rgb(0, 255, 166)';
   }
 }
 
