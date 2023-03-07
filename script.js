f7bbe8cc7d90eebb5306e198ffc3a6161a3a9328
kobe shaw <kobeshaw330@gmail.com>
2023-03-06 18:31:24 -0600

    added and fixed skill ball so i look more to match the vibe of the page

--- script.js
+++ script.js
@@ -80,7 +80,6 @@ function Light(){
     document.getElementById('modalAbo').style.color='black';
     document.getElementById('modalPro').style.color='black';
     document.getElementById('modalCon').style.color='black';
-    document.getElementsByClassName('tags-cloud').style.color='black';
   }
 }
 
