7e71058ad99732a7d447229fbb4db88d348edf90
kobe shaw <kobeshaw330@gmail.com>
2023-03-08 16:06:52 -0600

    fixed error in the console

--- script.js
+++ script.js
@@ -25,33 +25,33 @@ function Dark(){
   localStorage.setItem('Light', 'false');
   localStorage.setItem('Dark', 'true');
   if(localStorage.getItem('Dark') === 'true'){
-  document.body.style.backgroundColor= "#1c1c2c";
-  document.getElementById("filter").style.opacity = .55;
-  document.getElementById('line2').style.color='rgb(0, 255, 166)';
-  document.getElementById('line1').style.color='white';
-  document.getElementById('line3').style.color='white';
-  document.getElementById('paragraph').style.color='white';
-  document.getElementById('modal').style.backgroundColor='rgba(50, 48, 69, 0.994)';
-  document.getElementById('tags-cloud').style.color='rgb(0, 255, 166)';
-  document.getElementById('wannaTalk').style.color='white';
-  document.getElementById('overlayArrow2').style.color='rgb(0, 255, 166)';
-  document.getElementById('overlayArrow1').style.color='rgb(0, 255, 166)';
-  document.getElementById('overlayTitle1').style.color='rgb(0, 255, 166)';
-  document.getElementById('overlayTitle2').style.color='rgb(0, 255, 166)';
-  document.getElementById('overlayArrow3').style.color='rgb(0, 255, 166)';
+    document.body.style.backgroundColor= "#1c1c2c";
+    document.getElementById("filter").style.opacity = .55;
+    document.getElementById('line2').style.color='rgb(0, 255, 166)';
+    document.getElementById('line1').style.color='white';
+    document.getElementById('line3').style.color='white';
+    document.getElementById('paragraph').style.color='white';
+    document.getElementById('modal').style.backgroundColor='rgba(50, 48, 69, 0.994)';
+    document.getElementById('tags-cloud').style.color='rgb(0, 255, 166)';
+    document.getElementById('wannaTalk').style.color='white';
+    document.getElementById('overlayArrow2').style.color='rgb(0, 255, 166)';
+    document.getElementById('overlayArrow1').style.color='rgb(0, 255, 166)';
+    document.getElementById('overlayTitle1').style.color='rgb(0, 255, 166)';
+    document.getElementById('overlayTitle2').style.color='rgb(0, 255, 166)';
+    document.getElementById('overlayArrow3').style.color='rgb(0, 255, 166)';
     document.getElementById('overlayTitle3').style.color='rgb(0, 255, 166)';
-  document.getElementById('ViewMore1').style.color='rgb(0, 255, 166)';
-  document.getElementById('ViewMore2').style.color='rgb(0, 255, 166)';
-  document.getElementById('footer-name').style.color='white';
-  document.getElementById('title1').style.color='rgb(0, 255, 166)';
-  document.getElementById('title2').style.color='rgb(0, 255, 166)';
-  document.getElementById('contact').style.color='rgb(0, 255, 166)';
-  document.getElementById('modalHom').style.color='rgb(0, 255, 166)';
-  document.getElementById('modalAbo').style.color='rgb(0, 255, 166)';
-  document.getElementById('modalPro').style.color='rgb(0, 255, 166)';
-  document.getElementById('modalCon').style.color='rgb(0, 255, 166)';
-  document.getElementById('logo').style.color='rgb(0, 255, 166)';
-  document.getElementById('hamburger-button').style.color='rgb(0, 255, 166)';
+    document.getElementById('ViewMore1').style.color='rgb(0, 255, 166)';
+    document.getElementById('ViewMore2').style.color='rgb(0, 255, 166)';
+    document.getElementById('footer-name').style.color='white';
+    document.getElementById('title1').style.color='rgb(0, 255, 166)';
+    document.getElementById('title2').style.color='rgb(0, 255, 166)';
+    document.getElementById('contact').style.color='rgb(0, 255, 166)';
+    document.getElementById('modalHom').style.color='rgb(0, 255, 166)';
+    document.getElementById('modalAbo').style.color='rgb(0, 255, 166)';
+    document.getElementById('modalPro').style.color='rgb(0, 255, 166)';
+    document.getElementById('modalCon').style.color='rgb(0, 255, 166)';
+    document.getElementById('logo').style.color='rgb(0, 255, 166)';
+    document.getElementById('hamburger-button').style.color='rgb(0, 255, 166)';
     document.getElementById('name').style.color='rgb(0, 255, 166)';
     document.getElementById('twitter').style.color='rgb(0, 255, 166)';
     document.getElementById('gitHub').style.color='rgb(0, 255, 166)';
@@ -122,7 +122,7 @@ window.onload = () => {
   const hamburgerButton = document.getElementById("hamburger-button");
   const modal = document.getElementById("modal");
   const closeButton = document.getElementById("close-button");
-  const animatedText = document.getElementById("animated-text");
+
   
   hamburgerButton.addEventListener("click", () => {
     modal.classList.add("modal-open");
